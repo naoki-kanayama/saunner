b5be5e2b71cc027b173044f28dd1944e0410bfd2
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-16 15:43:53 +0900

    ログイン画面他微調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  resources :users, only: [:show, :edit, :update]
   resources :posts do
     resources :comments, only: :create
   end
   get '/posts/prefecture/:id', to: "posts#prefecture"
-  resources :users, only: [:show, :edit, :update]
   post '/like/:post_id' => 'likes#like', as: 'like'
   delete '/like/:post_id' => 'likes#unlike', as: 'unlike'
 end
